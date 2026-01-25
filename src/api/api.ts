124ec6cca02423fb44656b05088f214712de10a0
Lagerq2 <eliaslagerqvist@outlook.com>
2026-01-25 20:27:01 +0100

    fix med url

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,7 +11,7 @@ class ApiClient {
 
     constructor() {
         this.client = axios.create({
-            baseURL: import.meta.env.VITE_API_BASE_URL as string,
+            baseURL: import.meta.env.VITE_API_URL as string,
         });
 
         this.client.interceptors.request.use(this.attachAuthToken);
